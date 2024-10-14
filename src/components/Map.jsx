b2376869e4d3fee6e84321429f9992a69996043b
AbdulrahmanLuqman <luqmanola60@gmail.com>
2024-10-14 11:57:39 +0100

    added location url

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -3,19 +3,19 @@ import 'leaflet/dist/leaflet.css'
 import PropTypes from "prop-types"
 import Loading from "./Loading"
 // import { useEffect, useState } from "react"
-// import { Icon } from "leaflet"
+import { Icon } from "leaflet"
 // import customIconUrl from "../assets/icon-arrow.svg"
 
 const Map = ({isLoaded, geolocation}) => {
 
   // console.log(geolocation)
 
+    // console.log(marker)
+    const customIcon = new Icon({
+      iconUrl: "https://api.iconify.design/material-symbols:location-on.svg",
+      iconSize: [38, 38]
+    })
     const marker = [geolocation.latitude, geolocation.longitude]
-    console.log(marker)
-    // const customIcon = new Icon({
-    //   iconUrl: "https://www.flaticon.com/free-icon/pin_484167?term=location&page=1&position=8&origin=search&related_id=484167",
-    //   iconSize: [38, 38]
-    // })
   return (
     <>
       {
@@ -25,7 +25,7 @@ const Map = ({isLoaded, geolocation}) => {
              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
               url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker position={marker}>
+          <Marker position={marker} icon={customIcon}>
             <Popup><h3>lol</h3></Popup>
           </Marker>
         </MapContainer>: <><Loading/></>
